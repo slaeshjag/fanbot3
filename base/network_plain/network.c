7691679f26d171466410597f32fefaac6e4c20d8
Steven Arnow <s@rdw.se>
2012-09-01 13:04:08 +0200

    Started a little on the bare network plugin. SSL version will be more hitech

---
+++ base/network_plain/network.c
@@ -0,0 +1,60 @@
+#include "network.h"
+#include <config/api.h>
+
+
+unsigned int pluginType() {
+	return PLUGIN_TYPE_NETWORK;
+}
+
+
+void *pluginConnect(const char *host, int port) {
+	NETWORK_PLAIN *connection;
+	struct sockaddr_in address;
+	struct hostent *hp;
+
+	if ((connection = malloc(sizeof(NETWORK_PLAIN))) == NULL)
+		configErrorPush("Unable to malloc");
+		return NULL;
+	}
+
+	if ((hp = gethostbyname(host)) == NULL) {
+		configErrorPush("Unable to resolve hostname");
+		vree(connection);
+		return NULL;
+	}
+
+	if ((connection->socket = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		configErrorPush("Unable to create a socket");
+		free(connection);
+		return NULL;
+	}
+
+	address.sin_family = AF_INET;
+	address.sin_port = htons(port);
+	address.sin_addr.s_addr = *(u_long *) hp->h_addr;
+	
+	if (connect(connection->socket, (struct sockaddr *) &address, sizeof(struct sockaddr)) == -1) {
+		configErrorPush("Unable to establish a connection");
+		free(connection);
+		return NULL;
+	}
+
+	/* TODO: Fixa non-blocking */
+
+	return connection;
+}
+
+
+int pluginSocketGet(NETWORK_PLAIN *connection) {
+	return connection->socket;
+}
+
+
+int pluginReadData(NETWORK_PLAIN *connection, char *buffer, int buffer_len) {
+	return recv(connection->socket, buffer, buffer_len, 0);
+}
+
+
+int pluginSendData(NETWORK_PLAIN *connection, char *buffer, int buffer_len) {
+	return send(connection->socket, buffer, buffer_len, 0);
+}
---
+++ base/network_plain/network.h
@@ -0,0 +1,16 @@
+#ifndef __NETWORK_H__
+#define	__NETWORK_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <sys/types.h>
+
+
+typedef struct {
+	int		socket;
+} NETWORK_PLAIN;
+
+
+#endif
