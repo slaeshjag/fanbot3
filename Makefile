f1fdbe27fd0a76fff79432cf1bb4bca0c1a36b4e
Steven Arnow <s@rdw.se>
2012-09-09 01:25:40 +0200

    Made an attempt at a message cut-off detector. Might work, might not

--- Makefile
+++ Makefile
@@ -1,8 +1,10 @@
 default:
 	mkdir -p bin
 	mkdir -p bin/base
+	mkdir -p bin/extra
 	cp skel/* bin/base
 	cd base && make
+	cd extra && make
 
 clean:
 	rm -Rf bin
---
+++ extra/messagelen/messagelen.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <config/api.h>
+
+
+unsigned int pluginType() {
+	return PLUGIN_TYPE_FILTER;
+}
+
+
+const char *pluginName() {
+	return "Messagelenght-warning";
+}
+
+
+void *pluginDestroy(void *handle) {
+	return NULL;
+}
+
+
+void *pluginDoInit(const char *network) {
+	return NULL;
+}
+
+
+void pluginFilter(void *handle, const char *from, const char *host, const char *command, const char *channel, const char *message) {
+	char buff[520];
+
+	if (strcmp(command, "PRIVMSG") != 0)
+		return;
+	
+	sprintf(buff, ":%s!%s PRIVMSG %s :%s\r\n", from, host, channel, message);
+	if (strlen(buff) >= 512) {
+		channel = ircGetIntendedChannel(channel, from);
+		sprintf(buff, "%s: That message was exactly the lenght that the IRC-protocol allows. It probably got cut off", from);
+		ircMessage(channel, buff);
+	}
+
+	return;
+}
