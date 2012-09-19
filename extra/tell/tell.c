3e2f040294c2bb41ecce3688645efc579280f6b9
Steven Arnow <s@rdw.se>
2012-09-19 10:40:54 +0200

    Added a tell-command

---
+++ extra/tell/tell.c
@@ -0,0 +1,240 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <config/api.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+
+struct MESSAGE_BUFFER {
+	int			id;
+	char			message[128];
+	char			who[128];
+	char			channel[512];
+	struct MESSAGE_BUFFER	*next;
+};
+
+typedef struct {
+	struct MESSAGE_BUFFER	*buffer;
+	const char		*network;
+	int			cnt;
+} MAIN;
+
+
+
+void messageBufferDestroy(MAIN *m) {
+	struct MESSAGE_BUFFER *buffer, *tmp;
+
+	buffer = m->buffer;
+	m->buffer = NULL;
+	while (buffer != NULL) {
+		timerDelete(buffer->id);
+		tmp = buffer->next;
+		free(buffer);
+		buffer = tmp;
+	}
+
+	return;
+}
+
+
+void messageBufferAdd(MAIN *m, const char *message, const char *who, const char *channel) {
+	struct MESSAGE_BUFFER *buffer;
+
+	if ((buffer = malloc(sizeof(struct MESSAGE_BUFFER))) == NULL)
+		return;
+	
+	strncpy(buffer->message, message, 128);
+	buffer->message[127] = 0;
+	strncpy(buffer->who, who, 128);
+	strncpy(buffer->channel, channel, 512);
+	buffer->id = m->cnt;
+	m->cnt++;
+
+	buffer->next = m->buffer;
+	m->buffer = buffer;
+
+	return;
+}
+
+
+void messageBufferDump(MAIN *m) {
+	struct MESSAGE_BUFFER *buffer;
+	char buff[256];
+	FILE *fp;
+	
+	mkdir("data/tell", S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+	sprintf(buff, "data/tell/%s", m->network);
+
+	if ((fp = fopen(buff, "w")) == NULL) {
+		configErrorPush("pling: Unable to dump the messsage buffer to file: Unable to create file");
+		return;
+	}
+
+	buffer = m->buffer;
+	while (buffer != NULL) {
+		fprintf(fp, "%s %s %s\n", buffer->channel, buffer->who, buffer->message);
+		buffer = buffer->next;
+	}
+
+	fclose(fp);
+	return;
+}
+
+
+void messageBufferRead(MAIN *m) {
+	FILE *fp;
+	char buff[256], channel[512], message[130], who[128];
+
+	sprintf(buff, "data/tell/%s", m->network);
+
+	if ((fp = fopen(buff, "r")) == NULL) {
+		configErrorPush("pling: Warning: Unable to open message buffer dump");
+		return;
+	}
+
+	while (!feof(fp)) {
+		fscanf(fp, "%s %s", channel, who);
+		fgets(message, 130, fp);
+		if (strchr(message, '\n'))
+			*(strchr(message, '\n')) = 0;
+		messageBufferAdd(m, &message[1], who, channel);
+	}
+
+	fclose(fp);
+	unlink(buff);
+
+	return;
+}
+
+
+void messageBufferDelete(MAIN *m, int id) {
+	struct MESSAGE_BUFFER *buffer, *old;
+
+	old = m->buffer;
+	buffer = old->next;
+	if (old->id == id) {
+		m->buffer = old->next;
+		timerDelete(old->id);
+		free(old);
+		return;
+	}
+	while (buffer != NULL) {
+		if (buffer->id == id) {
+			old->next = buffer->next;
+			timerDelete(buffer->id);
+			free(buffer);
+			return;
+		}
+		old = buffer;
+		buffer = buffer->next;
+	}
+
+	return;
+}
+
+
+void checkForMessages(MAIN *m, const char *from) {
+	struct MESSAGE_BUFFER *buffer;
+	char channel[256], message[512];
+	int i, c;
+
+	*channel = 0;
+	c = 0;
+	buffer = m->buffer;
+	while (buffer != NULL) {
+		if (strcmp(buffer->who, from) == 0) {
+			ircMessage(buffer->who, buffer->message);
+			if (*buffer->channel == '#')
+				sprintf(channel, buffer->channel);
+			i = buffer->id;
+			buffer = buffer->next;
+			messageBufferDelete(m, i);
+			c++;
+		} else
+			buffer = buffer->next;
+	}
+
+	if (*channel != 0) {
+		sprintf(message, "%s: %i messages sent in query", from, c);
+		ircMessage(channel, message);
+	}
+
+	return;
+}
+
+
+unsigned int pluginType() {
+	return PLUGIN_TYPE_FILTER;
+}
+
+
+const char *pluginName() {
+	return "tell";
+}
+
+
+void *pluginDestroy(void *handle) {
+	messageBufferDump(handle);
+	messageBufferDestroy(handle);
+	free(handle);
+
+	return NULL;
+}
+
+
+void *pluginDoInit(const char *network) {
+	MAIN *m;
+
+	if ((m = malloc(sizeof(MAIN))) == NULL)
+		return NULL;
+	m->buffer = NULL;
+	m->network = network;
+	m->cnt = 0;
+
+	messageBufferRead(m);
+
+	return m;
+}
+
+
+void pluginTimerPoke(void *handle, int id) {
+	return;
+}
+
+
+void pluginFilter(void *handle, const char *from, const char *host, const char *command, const char *channel, const char *message) {
+	char buff[520], to[520];
+	
+	if (handle == NULL)
+		return;
+
+	if (strcmp(command, "PRIVMSG") != 0)
+		return;
+	if (strstr(message, "<tell ") != message) {
+		checkForMessages(handle, from);
+		return;
+	}
+
+	channel = ircGetIntendedChannel(channel, from);
+	
+	message += strlen("<tell ");
+	*to = 0;
+
+	sscanf(message, "%s ", to);
+	
+	if ((message = strstr(message, " ")) == NULL) {
+		sprintf(buff, "%s: You need to leave a message!", from);
+	}
+	else {
+		message++;
+		sprintf(buff, "<%s> %s", to, message);
+		messageBufferAdd(handle, buff, to, channel);
+		sprintf(buff, "%s: Mkay, I'll pass that on to %s", from, to);
+	}
+
+	ircMessage(channel, buff);
+
+	return;
+}
