61f839e191a219d673135a5d9620bb84a5aaa633
Steven Arnow <s@rdw.se>
2012-10-17 22:21:14 +0200

    Tried to improve the improvement to messagelen

--- extra/messagelen/messagelen.c
+++ extra/messagelen/messagelen.c
@@ -36,7 +36,8 @@ void pluginFilter(void *handle, const char *from, const char *host, const char *
 		return;
 	
 	sprintf(buff, ":%s!%s PRIVMSG %s :%s\r\n", from, host, channel, message);
-	message = &message[strlen(message) - 32];
+	message = &message[strlen(message) - 64];
+	message = (strstr(message, " ") != NULL) ? strstr(message, " ") + 1 : message;
 	if (strlen(buff) >= 512) {
 		channel = ircGetIntendedChannel(channel, from);
 		sprintf(buff, "%s: That message was exactly the length that the IRC-protocol allows. It probably got cut off. Last part of the message: »%s«", from, message);
