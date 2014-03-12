087dc7f6245b7ed9f05126c1bb75ce540fda9056
Steven Arnow <s@rdw.se>
2014-03-12 20:02:29 +0100

    Fixed memory corruption

--- extra/humour/humour.c
+++ extra/humour/humour.c
@@ -47,16 +47,16 @@ void *pluginDoInit(const char *network) {
 	if (!fp) {
 		m->list = NULL;
 		m->entries = 0;
+		return m;
 	}
 	
 	for (i = 0; !feof(fp); fgets(buff, 512, fp), i++);
 	i--;
-
 	m->list = malloc(sizeof(*m->list) * i);
 	m->entries = i;
 	fseek(fp, 0, SEEK_SET);
 
-	for (i = 0; !feof(fp); i++) {
+	for (i = 0; i < m->entries; i++) {
 		fscanf(fp, "%s %i ", buff, &m->list[i].args);
 		if ((t = strchr(buff, '@'))) {
 			*t = 0;
@@ -83,16 +83,19 @@ void pluginTimerPoke(void *handle, int id) {
 
 void pluginFilter(void *handle, const char *from, const char *host, const char *command, const char *channel, const char *message) {
 	char buff[520];
-	int i;
+	int i, j;
 	struct list_main *m = handle;
 
 	if (strcmp(command, "PRIVMSG") != 0)
 		return;
 	channel = ircGetIntendedChannel(channel, from);
 	for (i = 0; i < m->entries; i++) {
+		j = strlen(m->list[i].command);
 		/* TODO: Handle arguments */
-		if (strstr(message, m->list[i].command) == message)
-			ircMessage(channel, m->list[i].reply);
+		if (strstr(message, m->list[i].command) == message) {
+			if (message[j] == 0 || message[j] == ' ')
+				ircMessage(channel, m->list[i].reply);
+		}
 	}
 
 	return;
