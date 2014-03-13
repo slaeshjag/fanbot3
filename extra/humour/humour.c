5b6807119e38f0df76c85937629619bcf7068314
Steven Arnow <s@rdw.se>
2014-03-13 21:46:54 +0100

    Humour plugin can take arguments, fixed it up in general

--- extra/humour/humour.c
+++ extra/humour/humour.c
@@ -38,7 +38,7 @@ void *pluginDestroy(void *handle) {
 
 void *pluginDoInit(const char *network) {
 	struct list_main *m;
-	int i;
+	int i, j, args;
 	char *t, buff[128];
 	const char *nick = ircNickname();
 	FILE *fp = fopen("conf/humour.list", "r");
@@ -57,7 +57,7 @@ void *pluginDoInit(const char *network) {
 	fseek(fp, 0, SEEK_SET);
 
 	for (i = 0; i < m->entries; i++) {
-		fscanf(fp, "%s %i ", buff, &m->list[i].args);
+		fscanf(fp, "%s ", buff);
 		if ((t = strchr(buff, '@'))) {
 			*t = 0;
 			t++;
@@ -65,11 +65,27 @@ void *pluginDoInit(const char *network) {
 		} else
 			strcpy(m->list[i].command, buff);
 		fgets(m->list[i].reply, 320, fp);
+
+		for (j = args = 0; m->list[i].reply[j]; j++) {
+			if (m->list[i].reply[j] == '%') {
+				if (m->list[i].reply[j + 1] == '%')
+					j++;
+				else
+					args++;
+			}
+		}
+
+		if (args > 3) {
+			fprintf(stderr, "BAD MESSAGE IN LIST AT LINE %i: Too many arguments\n", i);
+			return NULL;
+		}
+
+		m->list[i].args = args;
 		m->list[i].reply[strlen(m->list[i].reply) - 1] = 0;
 		/* Allow for spaces in the command */
-		for (t = m->list[i].command; strchr(t, '~'); t = strchr(t, '%'))
+		for (t = m->list[i].command; t && strchr(t, '~'); t = strchr(t, '~'))
 			*strchr(t, '~') = ' ';
-		/* TODO: Expand something to the bots ircnick */
+		fprintf(stderr, "Added command '%s'\n", m->list[i].command);
 	}
 
 	return m;
@@ -82,7 +98,7 @@ void pluginTimerPoke(void *handle, int id) {
 
 
 void pluginFilter(void *handle, const char *from, const char *host, const char *command, const char *channel, const char *message) {
-	char buff[520];
+	char buff[520], arg1[512], arg2[512], arg3[512];
 	int i, j;
 	struct list_main *m = handle;
 
@@ -93,8 +109,25 @@ void pluginFilter(void *handle, const char *from, const char *host, const char *
 		j = strlen(m->list[i].command);
 		/* TODO: Handle arguments */
 		if (strstr(message, m->list[i].command) == message) {
-			if (message[j] == 0 || message[j] == ' ')
+			if (message[j] != 0 && message[j] != ' ')
+				continue;
+			message += strlen(m->list[i].command);
+			if (m->list[i].args > 0) {
+				*arg1 = *arg2 = *arg3 = 0;
+				sscanf(message, " %s %s %s", arg1, arg2, arg3);
+			}
+		
+			if (m->list[i].args == 0) {
 				ircMessage(channel, m->list[i].reply);
+				continue;
+			} else if (m->list[i].args == 1)
+				sprintf(buff, m->list[i].reply, arg1);
+			else if (m->list[i].args == 2)
+				sprintf(buff, m->list[i].reply, arg1, arg2);
+			else if (m->list[i].args == 3)
+				sprintf(buff, m->list[i].reply, arg1, arg2, arg3);
+			else continue;
+			ircMessage(channel, buff);
 		}
 	}
 
