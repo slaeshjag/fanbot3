49db3aafa9dafa3f3e4392a044604a2b815b9e39
Steven Arnow <s@rdw.se>
2014-04-05 22:49:27 +0200

    <day added!

---
+++ extra/day/day.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <time.h>
+#include <config/api.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#define	min(x, y)		((x) < (y) ? (x) : (y))
+
+
+void sendHelp(const char *from) {
+	ircMessage(from, "<day - Get current day and week");
+	return;
+}
+
+
+unsigned int pluginType() {
+	return PLUGIN_TYPE_FILTER;
+}
+
+
+const char *pluginName() {
+	return "day";
+}
+
+
+void *pluginDestroy(void *handle) {
+	return NULL;
+}
+
+
+void *pluginDoInit(const char *network) {
+	return NULL;
+}
+
+
+void pluginTimerPoke(void *handle, int id) {
+
+	return;
+}
+
+
+void pluginFilter(void *handle, const char *from, const char *host, const char *command, const char *channel, const char *message) {
+	char buff[520], ti[520];
+	time_t now;
+	struct tm t;
+
+	if (strcmp(command, "PRIVMSG") != 0)
+		return;
+	channel = ircGetIntendedChannel(channel, from);
+	
+	if (strcmp(message, API_HELP_CMD) == 0)
+		sendHelp(from);
+	if (strstr(message, "<day") == message) {
+		now = time(NULL);
+		localtime_r(&now, &t);
+		strftime(ti, 520, "%A %F, week %V", &t);
+		sprintf(buff, "%s: %s", from, ti);
+		ircMessage(channel, buff);
+	}
+	
+	return;
+}
